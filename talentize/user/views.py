a63f6f476790d450deeb885852fd086f7e24a8bb
Shankusu993 <pankaj08072000@gmail.com>
2020-05-16 15:16:50 +0530

    add error handling in AppleUserToProfile, and code style changes

--- talentize/user/views.py
+++ talentize/user/views.py
@@ -44,7 +44,8 @@ class Signup(APIView):
             )
             return Response({'message': 'success'}, status=status.HTTP_201_CREATED)
         except:
-            return Response({ 'message': 'invalid email'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message': 'invalid email'}, status=status.HTTP_400_BAD_REQUEST)
+
 
 class Login(APIView):
     '''
@@ -167,6 +168,7 @@ class AppleOAuth(APIView):
     POST endpoint to listen to redirect repsonse from Apple
     '''
     sub = ''
+
     def post(self, request, format=None):
         id_token = request.data['id_token']
         encoded_data = id_token.split('.')[1]
@@ -178,13 +180,14 @@ class AppleOAuth(APIView):
         except User.DoesNotExist:
             user = User()
             data_from_apple = request.data['user']
-            user.name = data_from_apple['name']['firstName'] + ' ' + data_from_apple['name']['lastName']
+            user.name = data_from_apple['name']['firstName'] + \
+                ' ' + data_from_apple['name']['lastName']
             user.email = data_from_apple['email']
             user.password_hash = sub
             user.save()
-        
+
     def get(self, request, format=None):
-        return (request, 'user/go_to_profile.html', { 'sub': sub })
+        return (request, 'user/go_to_profile.html', {'sub': sub})
         # button onclick => window.location.href = frontend.com/user/apple/sub/is_new_user
 
 
@@ -192,9 +195,12 @@ class AppleUserToProfile(APIView):
     '''
     POST request sent from frontend with data - sub
     '''
-    
+
     def post(self, request, sub, format=None):
-        user = User.objects.filter(password_hash=sub)
-        token = jwt.encode({'email': user.email, 'random': str(
-            datetime.now().timestamp())}, SECRET_FOR_JWT, algorithm='HS256').decode()
-        return Response({ 'message': 'success', 'token': token }, status = HTTP_200_OK)
+        try:
+            user = User.objects.filter(password_hash=sub)
+            token = jwt.encode({'email': user.email, 'random': str(
+                datetime.now().timestamp())}, SECRET_FOR_JWT, algorithm='HS256').decode()
+            return Response({'message': 'success', 'token': token}, status=HTTP_200_OK)
+        except:
+            return Response({'message': 'Invalid sub'}, status=status.HTTP_400_BAD_REQUEST)
