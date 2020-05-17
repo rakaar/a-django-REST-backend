51f5c67978aaddd87f6d1ca9b3feac2396205b36
Shankusu993 <pankaj08072000@gmail.com>
2020-05-17 17:46:11 +0530

    add Competitions and Patents to Profile model

--- talentize/user_profile/models.py
+++ talentize/user_profile/models.py
@@ -1,15 +1,20 @@
 from djongo import models
 
+
 class CollegeCourse(models.Model):
-    name = models.CharField(max_length=200) 
+    name = models.CharField(max_length=200)
+
 
 class Position(models.Model):
     year = models.CharField(max_length=10)
     description = models.CharField(max_length=200)
+
+
 class School(models.Model):
     name = models.CharField(max_length=200)
     board = models.CharField(max_length=200)
     percentage = models.CharField(max_length=10)
+
 
 class College(models.Model):
     name = models.CharField(max_length=200)
@@ -18,11 +23,14 @@ class College(models.Model):
     core_courses = models.ArrayField(model_container=CollegeCourse)
     additional_courses = models.ArrayField(model_container=CollegeCourse)
 
+
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=400)
-    project_type = models.CharField(max_length=10) #prof / FTP / BTP,MTP / self
-    associated_info = models.CharField(max_length=20, blank=True) # prof_name / college_name / prof_name / null
+    # prof / FTP / BTP,MTP / self
+    project_type = models.CharField(max_length=10)
+    # prof_name / college_name / prof_name / null
+    associated_info = models.CharField(max_length=20, blank=True)
 
 
 class ResearchPaper(models.Model):
@@ -33,29 +41,46 @@ class ResearchPaper(models.Model):
     is_main = models.BooleanField()
     name_of_main = models.CharField(max_length=200, blank=True)
 
+
 class PrevIntern(models.Model):
     company = models.CharField(max_length=200)
     job_title = models.CharField(max_length=200)
     from_date = models.CharField(max_length=200)
     to_date = models.CharField(max_length=200)
-    nature = models.CharField(max_length=10) # remote / direct
+    nature = models.CharField(max_length=10)  # remote / direct
+
 
 class PoR(models.Model):
-    place = models.CharField(max_length=200) # name of soceity
+    place = models.CharField(max_length=200)  # name of soceity
     positions = models.ArrayField(model_container=Position)
+
 
 class OnlineCourse(models.Model):
     name = models.CharField(max_length=200)
     company = models.CharField(max_length=200, blank=True)
     partner_insti = models.CharField(max_length=200, blank=True)
 
+
 class Certification(models.Model):
     name = models.CharField(max_length=200)
     year = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
 
+
 class Skill(models.Model):
     name = models.CharField(max_length=200)
+
+
+class Patent(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=600)
+    date = models.CharField(max_length=200)
+
+class Competition(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=600)
+    date = models.CharField(max_length=200)
+
 
 class Profile(models.Model):
     location = models.CharField(max_length=200)
@@ -70,4 +95,3 @@ class Profile(models.Model):
     competitions = models.ArrayField(model_container=Competition)
     certifications = models.ArrayField(model_container=Certification)
     skills = models.ArrayField(model_container=Skill)
-
