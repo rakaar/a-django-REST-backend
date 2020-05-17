ed0f03e0f140f03f95fbbed68169a49daa934d7d
Shankusu993 <pankaj08072000@gmail.com>
2020-05-17 23:38:06 +0530

    add url for Education endpoint

--- talentize/user_profile/urls.py
+++ talentize/user_profile/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
-url_patterns = [
+urlpatterns = [
     path('get_profile', views.Profile.as_view(), name='get_profile'),
+    path('update_education', views.Education.as_view(), name='education')
 ]
