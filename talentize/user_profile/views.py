9a63a18678f5fea569a90691b35f3da1dcfba95a
rakaar <rka87338@gmail.com>
2020-05-17 23:04:35 +0530

    Added endpoint to update Education Details

--- talentize/user_profile/views.py
+++ talentize/user_profile/views.py
@@ -23,3 +23,43 @@ class Profile(APIView):
         else:
             profile = user.profile
             return Response(profile, status=status.HTTP_200_OK)
+
+class Education(APIView):
+    '''
+    POST endpoint to update education details in profile
+    '''
+
+    def post(self, request, format=None):
+        '''
+        request.data.education : {
+            'college': {
+                'name': string,
+                'cgpa_range': string,
+                'dept': string,
+                'core_courses': [],
+                'additional_courses': []
+            },
+            'school': {
+                'name': string,
+                'board': string,
+                percentage: string
+            },
+            'online_courses': [
+                { 'name' : string, 'company': string, 'partner_insti': string }
+            ]
+        }
+        ''' 
+        email = request.data['email']
+        if not check_token(email, request.data['token']):
+            return Response({'message': 'invalid token'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            user = User.objects.filter(email=email)[0]
+        except User.DoesNotExist:
+            return Response({'message': 'invalid user'}, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            user.profile.college = request.data.education['college']
+            user.profile.school = request.data.education['school']
+            user.profile.online_courses = request.data.education['online_courses']
+            user.save()
+            return Response({ 'message': 'success'}, status=staus.HTTP_200_OK)
