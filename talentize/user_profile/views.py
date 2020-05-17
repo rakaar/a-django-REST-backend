68318ecf9bb6edbbafc1edd10c9d3bc23b9315b1
rakaar <rka87338@gmail.com>
2020-05-17 20:23:02 +0530

    Made small changes in get profile

--- talentize/user_profile/views.py
+++ talentize/user_profile/views.py
@@ -8,17 +8,18 @@ from user.utils import check_token
 
 class Profile(APIView):
     '''
-    GET endpoint to get/retrive profile details
+    GET endpoint to get all profile details
     '''
 
     def get(self, request, format=None):
         email = request.data['email']
         if not check_token(email, request.data['token']):
-            return Response({'message': 'token validation failed'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message': 'invalid token'}, status=status.HTTP_400_BAD_REQUEST)
+        
         try:
             user = User.objects.filter(email=email)[0]
         except User.DoesNotExist:
-            return Response({'message': 'Invalid User'}, status=status.HTTP_401_UNAUTHORIZED)
+            return Response({'message': 'invalid user'}, status=status.HTTP_401_UNAUTHORIZED)
         else:
             profile = user.profile
             return Response(profile, status=status.HTTP_200_OK)
