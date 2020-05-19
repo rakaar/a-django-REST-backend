ffe18e88bad9c2e57f4a595eb2247a54d1899f91
rakaar <rka87338@gmail.com>
2020-05-20 04:33:43 +0530

    Changed doc strings in utils check_token function

--- talentize/user/utils.py
+++ talentize/user/utils.py
@@ -5,8 +5,12 @@ SECRET_KEY_FOR_JWT = 'SECRET_KEY'
 
 def check_token(email, token):
     '''
-    utility function to verify 
-        email with email encoded in token
+    Utility function to verify email with email encoded in token
+    Args:
+        email(str): email of the user
+        token(str): token received after login
+    Returns:
+        True, if verification is successful afer decoding the token, False otherwise
     '''
     decode_obj = jwt.decode(token, SECRET_KEY_FOR_JWT, algorithms=['HS256'])
     return decode_obj['email'] == email
