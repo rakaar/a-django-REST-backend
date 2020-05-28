255792f825874f4f653d05e6f4d1173898274817
rakaar <rka87338@gmail.com>
2020-05-29 01:09:18 +0530

    Added mesibo fields to User Model

--- talentize/user/models.py
+++ talentize/user/models.py
@@ -8,6 +8,8 @@ class User(models.Model):
     insti_email = models.EmailField(blank=True, max_length=100)
     password_hash = models.CharField(blank=False, max_length=300)
     profile = models.EmbeddedField(blank=True, model_container=Profile)
-
+    mesibo_uid = models.CharField(blank=True, max_length=100)
+    mesibo_token = models.CharField(blank=True, max_length=100)
+    
     def __str__(self):
         return self.name
