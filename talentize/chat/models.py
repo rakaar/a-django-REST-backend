692288d74261f2a8961cdd019249ccdbac36a0ac
Shankusu993 <pankaj08072000@gmail.com>
2020-05-29 22:41:55 +0530

    updatechat  group model

--- talentize/chat/models.py
+++ talentize/chat/models.py
@@ -1,7 +1,11 @@
 from djongo import models
 
 
+class Mail(models.Model):
+    email = models.EmailField(blank=True)
+
+
 class Group(models.Model):
     gid = models.IntegerField(blank=False)
-    uni_ids = models.ArrayField(model_container=models.EmailField, blank=True)
+    uni_ids = models.ArrayField(model_container=Mail, blank=True)
     name = models.CharField(max_length=200, blank=True)
