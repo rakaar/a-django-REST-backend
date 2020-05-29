7c40d7d7d3c8998a58709377252700b3bf9d96db
Shankusu993 <pankaj08072000@gmail.com>
2020-05-29 20:52:34 +0530

    add models for chat feature

--- talentize/chat/models.py
+++ talentize/chat/models.py
@@ -1,3 +1,7 @@
-from django.db import models
+from djongo import models
 
-# Create your models here.
+
+class Group(models.Model):
+    gid = models.IntegerField(blank=False)
+    uni_ids = models.ArrayField(model_container=models.EmailField, blank=True)
+    name = models.CharField(max_length=200, blank=True)
--- talentize/chat/urls.py
+++ talentize/chat/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 
-url_patterns = [
-    path('group/', views.Group.as_view(), name='group')
+urlpatterns = [
+    path('group/', views.Group.as_view(), name='group'),
 ]
